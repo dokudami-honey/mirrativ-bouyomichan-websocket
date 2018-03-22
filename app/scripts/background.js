c220166efa95a6a36f7253bcecf8721ff4abd67c
dkdm hny <you@example.com>
2018-03-22 11:10:54 +0900

    use declarativeContent. remove tabs permission.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,30 +1,47 @@
 // Enable chromereload by uncommenting this line:
 // import 'chromereload/devonly'
-import { targetUrlPattern } from './modules/constants';
+// import { targetUrlPattern } from './modules/constants';
+import { pageMatcherOptions } from './modules/constants';
 import logger from './modules/log';
 const log = process.env.NODE_ENV === 'development' ? logger.debug : () => { };
-// マイグレーション的な?
+// /**
+//  * URLからプラグインの対象のページか否か判定
+//  * @param {*} url
+//  */
+// const matchUrl = (url) => {
+//   const targets = targetUrlPattern;
+//   for (const pattern of targets) {
+//     if (pattern.test(url)) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+const pageStateMatchers = pageMatcherOptions.map((option) => new chrome.declarativeContent.PageStateMatcher(option));
+
+// [
+//   new chrome.declarativeContent.PageStateMatcher({
+//     pageUrl: {
+//       hostEquals: 'www.mirrativ.com',
+//       pathPrefix: '/broadcast'
+//     }
+//   }),
+//   new chrome.declarativeContent.PageStateMatcher({
+//     pageUrl: {
+//       hostEquals: '192.168.33.30'
+//     }
+//   })
+// ]
+
 chrome.runtime.onInstalled.addListener((details) => {
   // console.log('previousVersion', details.previousVersion)
-});
-/**
- * URLからプラグインの対象のページか否か判定
- * @param {*} url
- */
-const matchUrl = (url) => {
-  const targets = targetUrlPattern;
-  for (const pattern of targets) {
-    if (pattern.test(url)) {
-      return true;
-    }
-  }
-  return false;
-};
-// ページが読み込まれたときに発生するっぼい
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  log('tabs.onUpdated:', tabId, ',changeInfo:', changeInfo, ',tab:', tab);
-  // 対象のページのみActionを有効にする
-  if (changeInfo.status === 'complete' && matchUrl(tab.url)) {
-    chrome.pageAction.show(tabId);
-  }
+  // log(chrome.declarativeContent);
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: pageStateMatchers,
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
 });
--- app/scripts/modules/constants.js
+++ app/scripts/modules/constants.js
@@ -1,9 +1,15 @@
+/*
+ * URL patterns.
+ */
 const mirrativBroadcastUrl = /https:\/\/www\.mirrativ\.com\/broadcast\/.*/;
 const targets = [mirrativBroadcastUrl];
 // development時のみローカル環境のURLを追加する。
 if (process.env.NODE_ENV === 'development') {
   targets.push(/http:\/\/192\.168\.33\.30\/.*/);
 }
+/*
+ * default option.
+ */
 export const DEFAULT_HOST = 'localhost';
 export const DEFAULT_PORT = 50002;
 export const defaultOptions = {
@@ -18,9 +24,24 @@ export const defaultOptions = {
   type: 0
 };
 export const targetUrlPattern = targets;
+/*
+ * for chrome.declarativeContent.PageStateMatcher用
+ */
+const options = [{
+  hostEquals: 'www.mirrativ.com',
+  pathPrefix: '/broadcast'
+}];
+// development時のみ開発環境のhostを追加する。
+if (process.env.NODE_ENV === 'development') {
+  options.push({
+    hostEquals: '192.168.33.30'
+  });
+}
+export const pageMatcherOptions = options;
 export default {
   // 対象のURL
-  targetUrlPattern: targetUrlPattern,
+  // targetUrlPattern: targetUrlPattern,
   // 設定値のデフォルト
-  defaultOptions: defaultOptions
+  defaultOptions: defaultOptions,
+  pageMatcherOptions: pageMatcherOptions
 };
